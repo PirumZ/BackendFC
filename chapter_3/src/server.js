ef7e13d71620ff875d5b0691e0209ebebe27bcd0
Nicholas <2105nfavazza@gmail.com>
2025-11-02 16:44:47 -0500

    DB WIP, does not work

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -1,1 +1,33 @@
-// This is test code, because I forget how git works
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+
+const app = express()
+const PORT  = process.env.PORT || 5000
+
+// Get file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url)
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+// MIddleware
+app.use(express.json())
+
+// Serves the HTML from the /public directory, also telling express to serve
+// all files from the folder as static assets
+
+app.use(express.static(path.join(__dirname, "../public")))
+
+// Servingg up the HTML from the /public directory
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+
+// Routes
+app.use('/auth', authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+})
