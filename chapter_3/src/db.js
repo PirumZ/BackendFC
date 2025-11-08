ddb0f252bce20d0a7e8ddda2d6fc3ee67d226174
Nicholas <2105nfavazza@gmail.com>
2025-11-08 12:33:03 -0500

    Chapter 3 Progress

--- chapter_3/src/db.js
+++ chapter_3/src/db.js
@@ -1,13 +1,13 @@
-import { DatabaseSync } from 'node:sqlite'
+import Database from 'better-sqlite3'
 
-const db = new DatabaseSync(':memory:')
+const db = new Database(':memory:')
 
 // Execute SQL statements from strings
 db.exec(`
     CREATE TABLE users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT UNIQUE,
+      password TEXT
     )
     `)
 
@@ -16,7 +16,7 @@ db.exec(`
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER,
             task TEXT,
-            completed BOOLEAN DEFAULT 0
+            completed BOOLEAN DEFAULT 0,
             FOREIGN KEY(user_id) REFERENCES users(id)
         )
     `)
--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -5,8 +5,35 @@ import db from '../db.js'
 
 const router = express.Router()
 
+// Register a new user ep /auth/register
 router.post('/register', (req, res) => {
-    
+    const { username, password } = req.body
+
+    const hashedPassword = bcrypt.hashSync(password, 8)
+
+    // save the new user and hashed password to the db
+    try{
+        const insertUser = db.prepare(`INSERT INTO users (username, password)
+            VALUES(?, ?)`)
+            const result = insertUser.run(username, hashedPassword)
+
+            // add a todo, given that we have a user
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task)
+            VALUES(?, ?)`)
+            insertTodo.run(result.lastInsertRowid, defaultTodo)
+
+        // create a token
+        const token = jwt.sign({id: result.lastInsertRowid},
+             process.env.JWT_SECRET,
+              {expiresIn: '24h'})
+              res.json({ token })
+
+    } catch(err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
+
 })
 
 router.post('/login', (req, res) => {
